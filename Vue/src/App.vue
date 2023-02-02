250c8029923a5fc262aa3db68677b4c67e755095
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-02-03 01:40:11 +0300

    code execution in Vue

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -2,15 +2,18 @@
   export default{
     data(){
       return{
-        text: 'Welcome to my website',
-        href: 'page.html',
+        num: 5,
+        num1: 1,
+        num2: 2,
+        num3: 3,
       }
     }
   }
 </script>
 
 <template>
-  <a href="page.html">{{text}}</a>
+  {{num * num}} 
+  {{num1 + num2 + num3}}
 </template>
 
 <style scoped>
