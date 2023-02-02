4ade8a8b10976b139170a2ae695ae690711470fd
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-02-02 21:00:05 +0300

    structure component vue

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,20 +1,14 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  export default{
+    name:'App',
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div> 
+    <h1> Hi, this page is for learning Vue </h1>
+    <p> I will use it to perform tasks </p>
+  </div>
 </template>
 
 <style scoped>
