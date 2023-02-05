3609fbf673a05b0f71f6ad03aa85569a1c5de1c1
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-02-05 21:36:48 +0300

    helper method

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,23 +1,65 @@
 <script>
-  export default {
-    data(){
-      return{
-        num1: 1,
-        num2: 2,
-      }      
+import { normalizeProps } from 'vue';
+export default {
+    data() {
+	    return {
+            selected: 'Не выбрано ',
+	    }
     },
-    methods: {
-      show: function(){
-        alert(this.num1+ this.num2);
+	methods: {
+	show: function() {
+		let selected1 = this.cape(this.selected);
+		alert('Выбранный день по счету это ' + selected1);
+        
+        let date = new Date();
+        let dayWeek = ['7', '1', '2', '3', '4', '5', '6'][date.getDay()];
+        alert('А сегодня - ' + this.cape(dayWeek)); 
+	},
+	cape: function(day_number) {
+		switch (day_number) {
+      case '1':
+        return (day_number = 'Понедельник')
+        break
+      case '2':
+        return (day_number = 'Вторник')
+        break
+      case '3':
+        return (day_number = 'Среда')                
+        break
+      case '4':
+        return (day_number = 'Четверг')
+        break
+      case '5':
+        return (day_number = 'Пятница')
+        break
+      case '6':
+        return (day_number = 'Суббота')
+        break
+      case '7':
+        return (day_number = 'Воскресенье')
+        break
       }
     }
-  }
+	}
+}
 </script>
 
 <template>
-    {{ show() }}
+    <div>
+    <select v-model="selected">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>     
+        <option>4</option>     
+        <option>5</option>     
+        <option>6</option>     
+        <option>7</option>      
+    </select>
+        <p><span>Номер дня недели по счету: {{selected}} </span></p>
+        <button @click="show">Показать название дня</button>
+    </div>
 </template>
-a
+
 <style scoped>
 header {
   line-height: 1.5;
